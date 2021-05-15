9e3e72f2ca03dbd10ee3bd7d45284112843e04fb
gmldlr12 <67935690+gmldlr12@users.noreply.github.com>
2021-05-15 09:13:46 +0900

    메인페이지 삽입

--- src/main/java/itc/hoseo/springproject/controller/MemberController.java
+++ src/main/java/itc/hoseo/springproject/controller/MemberController.java
@@ -1,9 +1,13 @@
 package itc.hoseo.springproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MemberController {
-
+    @RequestMapping("/main")
+    public String mainForm(){
+        return "main";
+    }
 	
 }
