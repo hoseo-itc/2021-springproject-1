1cea94f426b5f1914bad5ac402c622b3f8d24853
gmldlr12 <67935690+gmldlr12@users.noreply.github.com>
2021-05-15 10:20:47 +0900

    css/js/images 디렉토리 추가

--- src/main/java/itc/hoseo/springproject/controller/MemberController.java
+++ src/main/java/itc/hoseo/springproject/controller/MemberController.java
@@ -1,12 +1,14 @@
 package itc.hoseo.springproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MemberController {
-    @RequestMapping("/main")
-    public String mainForm(){
+
+    @RequestMapping(value = "/main")
+    public String main(){
         return "main";
     }
 	
