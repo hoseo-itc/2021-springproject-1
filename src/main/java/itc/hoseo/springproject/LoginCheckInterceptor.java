52dd58e490f039844adf1efbc5f08cef2b2f3420
gmldlr12 <67935690+gmldlr12@users.noreply.github.com>
2021-05-22 09:51:25 +0900

    로그인 체크 예외 추가 (/login/*,/css/*,/js/*,/images/*) 카카오 이미지 추가

--- src/main/java/itc/hoseo/springproject/LoginCheckInterceptor.java
+++ src/main/java/itc/hoseo/springproject/LoginCheckInterceptor.java
@@ -1,5 +1,6 @@
 package itc.hoseo.springproject;
 
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +11,11 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if(request.getRequestURI().equals("/login")) return true;
+        AntPathMatcher pathMatcher = new AntPathMatcher();
+        if(pathMatcher.match("/login/*",request.getRequestURI()))return true;
+        if(pathMatcher.match("/images/*",request.getRequestURI()))return true;
+        if(pathMatcher.match("/css/*",request.getRequestURI()))return true;
+        if(pathMatcher.match("/js/*",request.getRequestURI()))return true;
 
         HttpSession session = request.getSession(true);
 
