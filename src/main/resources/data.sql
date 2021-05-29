52490ca82bcb6177ee8e053fa7740e2aaa2dc229
gmldlr12 <67935690+gmldlr12@users.noreply.github.com>
2021-05-29 11:33:50 +0900

    post detail2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,72 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 
+-- 메니저 계정
 insert into Member(id,name,password,user_mode)
-values ('manager','매니저','q1w2e3r4',20);
+values ('manager1','매니저1','q1w2e3r4',20);
 
 insert into Member(id,name,password,user_mode)
-values ('testuser','테스터','q1w2e3r4',10);
+values ('manager2','매니저2','q1w2e3r4',20);
+
+insert into Member(id,name,password,user_mode)
+values ('manager3','매니저3','q1w2e3r4',20);
+
+-- 유저계정
+insert into Member(id,name,password,user_mode)
+values ('testuser1','테스터1','q1w2e3r4',10);
+
+insert into Member(id,name,password,user_mode)
+values ('testuser2','테스터2','q1w2e3r4',10);
+
+insert into Member(id,name,password,user_mode)
+values ('testuser3','테스터3','q1w2e3r4',10);
+
+insert into Member(id,name,password,user_mode)
+values ('testuser4','테스터4','q1w2e3r4',10);
+
+insert into Member(id,name,password,user_mode)
+values ('testuser5','테스터5','q1w2e3r4',10);
+
+insert into Member(id,name,password,user_mode)
+values ('testuser6','테스터6','q1w2e3r4',10);
+
+insert into Member(id,name,password,user_mode)
+values ('testuser7','테스터7','q1w2e3r4',10);
+
+insert into Member(id,name,password,user_mode)
+values ('testuser8','테스터8','q1w2e3r4',10);
+
+insert into Member(id,name,password,user_mode)
+values ('testuser9','테스터9','q1w2e3r4',10);
+
+insert into Member(id,name,password,user_mode)
+values ('testuser0','테스터0','q1w2e3r4',10);
+
+-- 작성글
+insert into Post(publisher_no,title)
+values (1,'테스트1');
 
 insert into Post(publisher_no,title)
-values (1,'테스트');
+values (1,'테스트2');
+
+insert into Post(publisher_no,title)
+values (1,'테스트3');
+
+insert into Post(publisher_no,title)
+values (2,'테스트4');
+
+insert into Post(publisher_no,title)
+values (2,'테스트5');
+
+insert into Post(publisher_no,title)
+values (2,'테스트6');
+
+insert into Post(publisher_no,title)
+values (3,'테스트7');
+
+insert into Post(publisher_no,title)
+values (3,'테스트8');
+
+insert into Post(publisher_no,title)
+values (3,'테스트9');
 
 
